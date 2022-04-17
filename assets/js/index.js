9cebf213895d9133277af9d8ca02b91baca6e54b
Awenca <84831770+Awenca@users.noreply.github.com>
2022-04-18 01:48:24 +0300

    Added calcs version 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,10 @@
-
 function sumInput (){
     const arr = [];
     while (true) {
         let x = +prompt ('Введите число');
-        if (!x) {
+        if ((!x) || (typeof x !=="number")) {
             break;
         }
-            else if (typeof x !=="number") {
-                break;
-            }
             else
                 arr.push (x);
                 console.log (arr);
@@ -21,4 +17,47 @@ function sumInput (){
     }
     console.log (sumA);
     document.querySelector('#sumA').innerHTML = sumA;
+}
+
+function calcMoto (){
+    const prise = document.querySelector("#priseMoto").value;
+    const volume = document.querySelector ("#volumeMoto").value;
+    const power = document.querySelector("#powerMoto").value;
+    const typeEngine = document.getElementsByName ('typeEngine');
+    const age = document.getElementsByName ('ageMoto');
+    const service = document.querySelector('#accept:checked');
+    for (let i=0; i<typeEngine.length; i++) {
+        if (typeEngine[i].checked && i<= 1) {
+            for (let i=0; i<age.length; i++) {
+                if (age[i].checked && i==0) {
+                    let bid = 1.5*volume;
+                    if (prise <= 2500) {
+                        let customs = 1550;
+                    }
+                    if (prise > 15000) {
+                        let customs = 8530;
+                    }
+                    let customs = 3100;
+                    let recycle = 5200;
+                    let result = customs + bid + recycle;
+                    }
+                if (age[i].checked && i>=1) {
+                    let bid = 3*volume;
+                    if (prise <= 2500) {
+                        let customs = 1550;
+                    }
+                    if (prise > 15000) {
+                        let customs = 8530;
+                    }
+                    let customs = 3100;
+                    let recycle = 5200;
+                    let result = customs + bid + recycle;
+                    }
+            }
+        }
+        let result = 0.15*prise + 511*power + 0.2*prise + 3400;
+        console.log (result);
+        document.querySelector('#sumMoto').innerHTML = result;
+    }
+
 }
