dbd79cb80fe393492708a906d996c01d79570a1a
Awenca <84831770+Awenca@users.noreply.github.com>
2022-04-25 00:39:50 +0300

    Added calcs and task3

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,45 +19,85 @@ function sumInput (){
     document.querySelector('#sumA').innerHTML = sumA;
 }
 
-function calcMoto (){
-    const prise = document.querySelector("#priseMoto").value;
+function calcMoto(){
+    let prise = document.querySelector("#priseMoto").value;
     const volume = document.querySelector ("#volumeMoto").value;
     const power = document.querySelector("#powerMoto").value;
+    const currency = document.querySelector ('#accept:checked');
     const typeEngine = document.getElementsByName ('typeEngine');
     const age = document.getElementsByName ('ageMoto');
     const service = document.querySelector('#accept:checked');
-    for (let i=0; i<typeEngine.length; i++) {
-        if (typeEngine[i].checked && i<= 1) {
-            for (let i=0; i<age.length; i++) {
-                if (age[i].checked && i==0) {
-                    let bid = 1.5*volume;
-                    if (prise <= 2500) {
-                        let customs = 1550;
-                    }
-                    if (prise > 15000) {
-                        let customs = 8530;
-                    }
-                    let customs = 3100;
-                    let recycle = 5200;
-                    let result = customs + bid + recycle;
-                    }
-                if (age[i].checked && i>=1) {
-                    let bid = 3*volume;
-                    if (prise <= 2500) {
-                        let customs = 1550;
-                    }
-                    if (prise > 15000) {
-                        let customs = 8530;
-                    }
-                    let customs = 3100;
-                    let recycle = 5200;
-                    let result = customs + bid + recycle;
-                    }
-            }
+
+    // Пересчет в валюте
+    if (currency.value == "82") {
+        let prise = prise/82;
+    }
+    else if (currency.value == 0.93) {
+        let prise = prise*0.93;
+    }
+    // Таможенное оформление
+        if (prise < 2500) {
+            let customs = 1550;
         }
-        let result = 0.15*prise + 511*power + 0.2*prise + 3400;
+        else if (prise >=2500 && prise <5000) {
+            let customs = 3100;
+        }
+        else {
+            let customs = 8530;
+        }
+    // Выбор типа двигателя
+        if (typeEngine == null) { alert ('Укажите тип двигателя');}
+        else if (typeEngine.value == "Бензин" || typeEngine.value == "Дизель") {
+            let recycle = 5200; //утилизационный сбор
+            // Проверка возраста
+            if (age == null) { alert ('Укажите возраст');}
+            else if (age.value == "меньше 5 лет") {
+                let bid = 1.5*volume; //единая ставка
+            }
+            else if (age.value == "от 5 до 7 лет" || age.value == "больше 7") {
+                let bid = 3*volume;
+            }
+            let expenses = customs + bid + recycle;
+        }
+        else {
+            let expenses = 0.15*prise + 511*power + 0.2*prise + 3400;
+        }
+        if (service == null) {
+            let result = expenses;
+        }
+        else if (service.value == "С сопровождением по России") {
+            let result = expenses + 2000;
+        }
+        else if (service.value == "С сопровождением по Японии") {
+            let result = expenses + 1000;
+        }
+        else {
+            let result = expenses + 3000;
+        }
         console.log (result);
         document.querySelector('#sumMoto').innerHTML = result;
-    }
+}
 
+function array2 () {
+    let arr2 = ['js', 'css', 'html'];
+    console.log (arr2[0]);
+}
+
+function array3 (){
+    let arr3 = [0, 1, false, 2, undefined, '', 3, null];
+    let Newarr3 = arr3.filter(function(number) {
+        return number > 0;
+    });
+    console.log (Newarr3);
+}
+
+function array4 () {
+    let arr4 = [[1,2], [1,2,3], [1,2,3,4]];
+    for (i=0; i<arr4.length; i++) {
+        let Newarr4 = arr4[i];
+        if (Newarr4.length>3){
+            let result = i;
+        }
+    }
+console.log (result);
 }
