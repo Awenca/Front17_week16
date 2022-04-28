f1d5299e7b29cbadab21013da8138fa0db8da211
Awenca <84831770+Awenca@users.noreply.github.com>
2022-04-28 23:32:53 +0300

    Chaged index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,43 +27,41 @@ function calcMoto(){
     const typeEngine = document.getElementsByName ('typeEngine');
     const age = document.getElementsByName ('ageMoto');
     const service = document.getElementsByName('serviceMoto').value;
-
-    // Пересчет в валюте
-    // let prise = (currency == null) ? alert ('Укажите валюту'):
-    //     (currency.value == "82") ? prise/82:
-    //     (currency.value == 0.93) ? prise*0.93:
-    //     prise *1;
+    let expenses = 0;
+    let customs = 0;
+    let result = 0;
 
     if (!currency) { alert ('Укажите валюту');}
-    else prise *= +currency;
+    else {prise *= +currency};
     // Таможенное оформление
-        let customs = '';
-        if (prise < 2500) {
-            let customs = 1550;
-        }
-        else if (prise >=2500 && prise <5000) {
-            let customs = 3100;
-        }
-        else {
+    if (prise < 2500) {
+        let customs = 1550;
+    }
+    else if (prise >=2500 && prise <5000) {
+        let customs = 3100;
+    }
+    else {
             let customs = 8530;
         }
     // Выбор типа двигателя
-    let expenses = '';
-        if (typeEngine == null) { alert ('Укажите тип двигателя');}
-        else if (typeEngine.value == "Бензин" || typeEngine.value == "Дизель") {
-            let recycle = 5200; //утилизационный сбор
+
+    if (typeEngine == null) {
+        alert ('Укажите тип двигателя');
+    }
+    else if (typeEngine.value == "Бензин" || typeEngine.value == "Дизель") {
+        let recycle = 5200; //утилизационный сбор
             // Проверка возраста
-            if (age == null) { alert ('Укажите возраст');}
-            else if (age.value == "меньше 5 лет") {
-                let bid = 1.5*volume; //единая ставка
-            }
-            else if (age.value == "от 5 до 7 лет" || age.value == "больше 7") {
-                let bid = 3*volume;
-            }
-            let expenses = customs + bid + recycle;
+        if (age == null) { alert ('Укажите возраст');}
+        else if (age.value == "меньше 5 лет") {
+            let bid = 1.5*+volume; //единая ставка
+        }
+        else if (age.value == "от 5 до 7 лет" || age.value == "больше 7") {
+            let bid = 3*+volume;
+        }
+        let expenses = +customs + +bid + +recycle;
         }
         else {
-            let expenses = 0.15*prise + 511*power + 0.2*prise + 3400;
+            let expenses = 0.15*+prise + 511*+power + 0.2*+prise + 3400;
         }
         if (!service) { result = expenses;}
         else result = +expenses + +service;
